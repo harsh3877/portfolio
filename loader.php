fecd09babfd1d01d18b0a6344fee7d6becd3d3ea
harsh3877 <67279685+harsh3877@users.noreply.github.com>
2021-06-25 12:32:45 +0530

    Update loader.php

--- loader.php
+++ loader.php
@@ -1,5 +1,14 @@
 <div class="container-fluid  loadercontainer" id="loadercontainer">
-  <div class="row m-0 w-100 h-100 justify-content-center align-items-center">
-	<img src="image/loader.gif">
+  <div class="row m-0 w-100 h-100 justify-content-center align-items-center ">
+	<!-- <img src="image/loader.gif"> -->
+	<div class="mx-auto" style="margin-top: -120px;">
+		<h2 id="animate">Welcome</h2>
+		<hr id="counterline">
+	</div>
+	
+  <div class="counter">
+		   <h2><span class="digits">0</span>%</h2>
+		   
+	</div>
   </div>
 </div>
