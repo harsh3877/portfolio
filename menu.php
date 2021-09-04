57870a3ecd0e31344d074ab3f8c1263f1425247c
harsh3877 <67279685+harsh3877@users.noreply.github.com>
2021-09-04 19:38:44 +0530

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,35 +1,36 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="sideNav">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				  <span class="navbar-toggler-icon col-6">  </span>
-				</button>
-				<a class="navbar-brand js-scroll-trigger" href="#page-top">
-				  <span class="d-block d-lg-none">
-				  <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="image/harsh.png" alt="" width="50px"> Harsh Sharma
-				  </span>
-				  <span class="d-none d-lg-block">
-					<img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="image/harsh.png" alt="">
-				  </span>
-				</a>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				  <ul class="navbar-nav">
-				  <li class="nav-item">
-					  <a class="nav-link js-scroll-trigger" href="resume/harshsharma's_resume.pdf" target="_blank">Get Resume</a>
-					</li>
-					<li class="nav-item">
-					  <a class="nav-link js-scroll-trigger"  href="#about">About</a>
-					</li>
-					<li class="nav-item">
-					  <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-					</li><li class="nav-item">
-					  <a class="nav-link js-scroll-trigger" href="#clientrelation">Clients</a>
-					</li>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon col-6">  </span>
+	</button>
+	<a class="navbar-brand js-scroll-trigger" href="#page-top">
+	  <span class="d-block d-lg-none mr-2">
+	  <img class="img-fluid img-profile rounded-circle mx-auto" src="image/harsh.png" alt="" width="50px"> Harsh Sharma
+	  </span>
+	  <span class="d-none d-lg-block">
+	  	<!-- image previous css 'img-fluid img-profile rounded-circle mx-auto mb-2'-->
+		<img class="myPic" src="image/harsh.png" alt="">
+	  </span>
+	</a>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	  <ul class="navbar-nav">
+	  <li class="nav-item">
+		  <a class="nav-link js-scroll-trigger" href="resume/harshsharma's_resume.pdf" target="_blank">Get Resume</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link js-scroll-trigger"  href="#about">About</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+		</li><li class="nav-item">
+		  <a class="nav-link js-scroll-trigger" href="#clientrelation">Clients</a>
+		</li>
 
-					<li class="nav-item">
-					  <a class="nav-link js-scroll-trigger"  href="#internship">Projects</a>
-					</li>
-					<li class="nav-item">
-					  <a class="nav-link js-scroll-trigger"  href="#contact">Contact Me</a>
-					</li>
-				 </ul>
-				</div>
-			</nav>
+		<li class="nav-item">
+		  <a class="nav-link js-scroll-trigger"  href="#internship">Projects</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link js-scroll-trigger"  href="#contact">Contact Me</a>
+		</li>
+	 </ul>
+	</div>
+</nav>
