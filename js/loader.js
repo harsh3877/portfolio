af5a088412b6a7281cfd878fc6f911bb95c3d48e
naga <na@gc.com>
2022-04-27 13:52:49 +0530

    'ads'

--- js/loader.js
+++ js/loader.js
@@ -4,29 +4,62 @@ AOS.init({
     }); 
 
 var nameInterval;
+
+var freeVar = 10;
+var finalCount=100;
   (function(){
         document.getElementById('loadercontainer').style.display="block";   
         document.getElementById('page-top').style.overflowY="hidden";
+
          var counter = 0;
       var c = 0;
-      var i = setInterval(function(){
-          $(".counter h2 span").html(c);
-          $("#counterline").css("width", c + "%");
-         
-        counter++;
-        c++;
-          
-        if(counter == 101) {
-            clearInterval(i);
-            document.getElementById('page-top').style.overflowY="auto";
-           
-            document.getElementById('loadercontainer').style.display="none";
-          nameInterval = setInterval(autotype, 100);
-        }
-         document.getElementById('loadercontainer').style.backdropFilter="blur("+(100-c)+"px)";
-        
-      }, 25);
+      var i = setInterval(loadingScreen, 200);
+
+        function loadingScreen(){
+            counter++;
+            c++;
+              $("#counter span").html(c);
+
+            if(counter<freeVar){
+              $(".status h2 span").html('Processing..');
+            }
+              $("#counterline").css("width", c + "%");
+             
+
+            if(counter==freeVar){
+              clearInterval(i);
+              i= setInterval(loadingScreen, 20);
+              // $(".status h2 span").attr('data-aos','fade-up');
+               $(".status h2 span").html('Almost done..');
+            }
+
+            if(counter==70){
+              var changes = $(".loadercontainer");
+              changes.css("border-bottom-right-radius","50px")
+              changes.css("border-bottom-left-radius","50px")
+
+            }
+            if(counter==90){
+
+               $(".loadercontainer").attr('data-aos','fade-down');
+            }
+
+            if(counter == finalCount) {
+                clearInterval(i);
+                
+                document.getElementById('page-top').style.overflowY="auto";
+               
+               setTimeout(()=>{
+                document.getElementById('loadercontainer').style.display="none";
+               },400)
+              nameInterval = setInterval(autotype, 100);
+            }
+             document.getElementById('loadercontainer').style.backdropFilter="blur("+(100-c)+"px)";
+            
+          }
+
   }());
+
 
   $(function () {
         $('[data-toggle="tooltip"]').tooltip()
--- loader.php
+++ loader.php
@@ -1,13 +1,20 @@
 <div class="container-fluid  loadercontainer" id="loadercontainer">
 	  <div class="innerLoader">
 		
-			<div class="" style="">
-					<h2 id="animate">Welcome</h2>
-					<hr id="counterline" style="width: 0px;">
+			<div style="display: flex;flex-direction: column;">
+					<h2 id="animate" style="color:#B2B2B2">Building Best Page For You</h2>
+					<div style="display: flex;align-items: center;">
+						<hr id="counterline" style="width: 0px;">
+						<span id=counter><span></span>%</span>
+					</div>
 			</div>
-		
-		  <div class="counter">
-					<h2><span class="digits">0</span>%</h2>
+			<div class="page-loader-container">
+				<img src="image/loading.gif" />
 			</div>
+		  <div class="status">
+					<h2><span class="startus" style="color:#72D053;"></span> </h2>
+			</div>
+
+
 	  </div>
 </div>
